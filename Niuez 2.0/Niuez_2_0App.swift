138667a5c0d657797129581b3bb0e22945b4bd46
Mohd Aamir <aamirmohd344@gmail.com>
2023-02-16 23:30:21 +0530

    Initial Commit

---
+++ Niuez 2.0/Niuez_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Niuez_2_0App.swift
+//  Niuez 2.0
+//
+//  Created by Mohd Aamir on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Niuez_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
